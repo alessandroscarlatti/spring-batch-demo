38f3654ee403d6491c0700f2baf622967f98c534
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-02-25 00:28:15 -0600

    rename integration profile to unitTest

--- src/test/java/com/scarlatti/springbatchdemo/BatchTests.java
+++ src/test/java/com/scarlatti/springbatchdemo/BatchTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles({"default", "integration"})
+@ActiveProfiles({"default", "unitTest"})
 public class BatchTests {
 
 	@Test
